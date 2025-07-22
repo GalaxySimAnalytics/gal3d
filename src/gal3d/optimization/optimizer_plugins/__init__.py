4e00e82ef8875dafadec50305e45689ca0d8954f
gal3d <2035180080@qq.com>
2025-07-22 22:16:16 +0800

    optimizer default

--- src/gal3d/optimization/optimizer_plugins/__init__.py
+++ src/gal3d/optimization/optimizer_plugins/__init__.py
@@ -1,3 +1,19 @@
-from .optimize_nlopt import OptimizerNLopt
-from .optimize_optimagic import OptimizerOptimagic
+
+# default optimizer
 from .optimize_scipy import OptimizerScipy
+
+# optional optimizers
+try:
+    from .optimize_nlopt import OptimizerNLopt
+except ImportError:
+    pass
+    
+try:
+    from .optimize_optimagic import OptimizerOptimagic
+except ImportError:
+    pass
+
+try:
+    from .optimize_emcee import OptimizerEmcee
+except ImportError:
+    pass

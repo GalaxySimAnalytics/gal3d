4a37250291ca2c02d521cf4d8c5dfa0ccc20184e
gal3d <2035180080@qq.com>
2026-03-06 19:04:06 +0800

    feat: add iterate ellipsoid for source density

--- src/gal3d/model_workflow/fit_workflow_plugins/__init__.py
+++ src/gal3d/model_workflow/fit_workflow_plugins/__init__.py
@@ -2,6 +2,7 @@
 Plugin implementations for fitting workflows, defining and manipulating various fitting procedures.
 """
 from .ellipsoid_fit import EllipsoidFitWorkflow
+from .iterate_ellipsoid_continuous import IterateEllipsoidDensity
 from .iterate_ellipsoid_discrete import IterateEllipsoidParticles
 
-__all__ =["EllipsoidFitWorkflow", "IterateEllipsoidParticles"]
+__all__ =["EllipsoidFitWorkflow", "IterateEllipsoidParticles", "IterateEllipsoidDensity"]
